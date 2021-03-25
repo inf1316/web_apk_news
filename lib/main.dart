1a04f18cb68ebcda19656b089aea1533d5140068
mclanghlin <mclanghlin@gmail.com>
2021-03-25 16:42:19 -0300

    building menu

--- lib/main.dart
+++ lib/main.dart
@@ -19,11 +19,49 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  List<Tab> _tabList = [
+    Tab(
+      child: Text("Portada"),
+    ),
+    Tab(
+      child: Text("Noticias"),
+    ),
+    Tab(
+      child: Text("Promociones"),
+    ),
+    Tab(
+      child: Text("Cartelera"),
+    ),
+    Tab(
+      child: Text("Pronosticos"),
+    ),
+    Tab(
+      child: Text("Perdidas-Hallazgo"),
+    ),
+  ];
+
+  TabController _tabController;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _tabController = TabController(length: _tabList.length, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        toolbarHeight: 110.0,
         leading: IconButton(
           onPressed: () {},
           icon: Icon(
@@ -35,8 +73,53 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
         title: Text(
           "Noticias",
-          style: TextStyle(color: Colors.blaack),
+          style: TextStyle(color: Colors.black),
         ),
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(30.0),
+          child: TabBar(
+            indicatorColor: Colors.black,
+            labelColor: Colors.black,
+            isScrollable: true,
+            controller: _tabController,
+            tabs: _tabList,
+          ),
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Container(
+              ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Container(
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Container(
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Container(
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Container(
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Container(
+            ),
+          ),
+        ],
       ),
     );
   }
